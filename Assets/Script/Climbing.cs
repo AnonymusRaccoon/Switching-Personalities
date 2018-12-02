ff712daa0be3fa0d415d4da672451e6a52d87fe6
Tristan Roux <tr.tristan-roux@outlook.fr>
2018-12-02 17:50:32 +0100

    Adding climbing cloud

---
+++ Assets/Script/Climbing.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Climbing : MonoBehaviour
+{
+    public float height = 10;
+    public float speed = 2;
+
+    private void Update()
+    {
+        transform.position = new Vector3(transform.position.x, height * Mathf.Sin(Time.time * speed), transform.position.y);
+    }
+}
